e900966bb9fea1975857dafa3e5905825a28a6fe
Jiyoung Baek <vvvv4449@gmail.com>
2022-08-24 03:09:27 +0900

    first commit

---
+++ ProgramersJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.programers_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramersJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramersJavaApplication.class, args);
+    }
+
+}
