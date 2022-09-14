3584e45b0a4f49653896005fc38094983d122fe4
Jiyoung Baek <vvvv4449@gmail.com>
2022-09-15 00:24:19 +0900

    수박수박수박수박수박수?

---
+++ Programers_Java/level1/_19_Palindrome/Solution.java
@@ -0,0 +1,38 @@
+package com.example.programers_java.Programers_Java.level1._19_Palindrome;
+
+/**
+ * 수박수박수박수박수박수?
+ * 문제 설명
+ * 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+ * 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+ *
+ * 제한 조건
+ * n은 길이 10,000이하인 자연수입니다.
+ * 입출력 예
+ * n	return
+ * 3	"수박수"
+ * 4	"수박수박"
+ */
+public class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                answer += "수";
+            }else {
+                answer += "박";
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+       Solution s = new Solution();
+        int n = 3;
+        String answer = s.solution(n);
+        System.out.println(answer);
+
+    }
+}
