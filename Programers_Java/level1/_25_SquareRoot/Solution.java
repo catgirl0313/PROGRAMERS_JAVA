f57f0d84acba5138edb8704c4114f735342095fd
Jiyoung Baek <vvvv4449@gmail.com>
2022-09-26 21:44:46 +0900

    정수 제곱근 판별

---
+++ Programers_Java/level1/_25_SquareRoot/Solution.java
@@ -0,0 +1,46 @@
+package com.example.programers_java.Programers_Java.level1._25_SquareRoot;
+
+/**
+ * 정수 제곱근 판별
+ * 문제 설명
+ * 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+ * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
+ * n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+ *
+ * 제한 사항
+ * n은 1이상, 50000000000000 이하인 양의 정수입니다.
+ * 입출력 예
+ * n	return
+ * 121	144
+ * 3	-1
+ * 입출력 예 설명
+ * 입출력 예#1
+ * 121은 양의 정수 11의 제곱이므로, (11+1)를 제곱한 144를 리턴합니다.
+ *
+ * 입출력 예#2
+ * 3은 양의 정수의 제곱이 아니므로, -1을 리턴합니다.
+ */
+public class Solution {
+
+    public long solution(long n) {
+
+        long x = (long) Math.sqrt(n); // n의 제곱근
+
+        if (n == x * x) {
+            return (long) ((x + 1) * (x + 1));
+        }else{
+            return -1;
+        }
+    }
+
+    public static void main(String[] args){
+        Solution c = new Solution();
+        long n = 121;
+
+        long answer = c.solution(n);
+        System.out.println(answer);
+
+    }
+
+
+}
