169b131bb858c04f75e9bbefd512f3be0bc34b48
Jiyoung Baek <vvvv4449@gmail.com>
2022-10-04 00:54:11 +0900

    하샤드 수

--- Programers_Java/level1/_28_Harshad_Number/Solution.java
+++ Programers_Java/level1/_28_Harshad_Number/Solution.java
@@ -3,7 +3,9 @@ package com.example.programers_java.Programers_Java.level1._28_Harshad_Number;
 /**
  * 하샤드 수
  * 문제 설명
- * 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+ * 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+ * 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다.
+ * 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
  *
  * 제한 조건
  * x는 1 이상, 10000 이하인 정수입니다.
@@ -28,7 +30,26 @@ package com.example.programers_java.Programers_Java.level1._28_Harshad_Number;
  */
 public class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        return answer;
+        int sum = 0;
+        int a = x;  // 전역변수 지역변수 확인하기!
+
+        while(a !=0){
+            sum += a % 10;
+            a /= 10;
+        }
+
+        if(x % sum == 0){
+            return true;
+        }else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Solution c = new Solution();
+        int x = 11;
+
+        boolean answer = c.solution(x);
+        System.out.println(answer);
+
     }
 }
